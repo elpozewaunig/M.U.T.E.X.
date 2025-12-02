56193bcd123c92f78d52b678e7271851a5060262
Philipp Wolfger <philipp.wolfger@alturos.com>
2025-12-02 14:45:53 +0100

    Fixed warnings in speed bar

--- scripts/speed_bar.gd
+++ scripts/speed_bar.gd
@@ -1,8 +1,6 @@
 extends TextureProgressBar
-var current_speed = 0
 @export var playerMovementNode: Node3D
 @export var player:CharacterBody3D
-var playerSpeedVar: int = 0
 @export var speedColor:Color
 @export var boostColor:Color
 @export var gradient: Gradient
@@ -12,14 +10,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	update_speed()
 
 func update_speed():
 	if not player:
 		return
-	playerSpeedVar = player.velocity.length()
-	value = playerSpeedVar
-	#if gradient and max_value > 0:
-		#var ratio = value / 120
-		#tint_progress = gradient.sample(ratio)
+	value = player.velocity.length()
